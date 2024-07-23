6c0492351388d67bca28f8530af4a49b1d435aab
JPratama7 <josepratama080@gmail.com>
2024-07-24 03:44:24 +0700

    feat: pindah ke helper

--- preprocess.py
+++ preprocess.py
@@ -3,24 +3,7 @@ import pandas as pd
 import argparse
 import sys
 
-from result import Result, Err, Ok
-
-
-def name_and_file_type(args: argparse.Namespace, key: str) -> Result[tuple[str, str], str]:
-    arg = vars(args)
-
-    if key not in arg:
-        return Err("Key not found")
-
-    arg_val = arg[key]
-
-    if arg_val.endswith('.xlsx'):
-        return Ok((arg_val, 'excel'))
-
-    if arg_val.endswith('.csv'):
-        return Ok((arg_val, 'csv'))
-
-    return Err("data type not supported")
+import helper
 
 def main():
     parser = argparse.ArgumentParser(description='Proses input dan output file.')
@@ -28,28 +11,30 @@ def main():
     parser.add_argument('-o', '--output', type=str, required=True, help='Path ke file output')
 
     if len(sys.argv) < 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+        helper.write_to_syserr(parser.print_help())
 
     args = parser.parse_args()
 
-    file_input = name_and_file_type(args, 'input')
-    output = name_and_file_type(args, 'output')
+    file_input = helper.name_and_file_type(args, 'input')
+    output = helper.name_and_file_type(args, 'output')
 
     if file_input.is_err():
-        sys.stderr.write(file_input.unwrap_err())
-        sys.exit(1)
+        helper.write_to_syserr(file_input.unwrap_err())
 
     if output.is_err():
-        sys.stderr.write(output.unwrap_err())
-        sys.exit(1)
+        helper.write_to_syserr(file_input.unwrap_err())
+
 
     df = pd.DataFrame()
 
-    # Normalize Kabupaten
-    df['Kabupaten'] = df['Kabupaten'].str.replace('Kab. ', '')
-    df['Kabupaten'] = df['Kabupaten'].str.replace('Kabupaten ', '')
+    match file_input.unwrap():
+        case "csv":
+            df = pd.read_csv(file_input.unwrap())
+        case "excel":
+            df = pd.read_excel(file_input.unwrap())
 
+
+    print(df)
 
 if __name__ == "__main__":
     main()
