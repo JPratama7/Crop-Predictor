b7e2e77151a5e3f3e688e6ed306d06dac0ef7a31
JPratama7 <josepratama080@gmail.com>
2024-07-24 04:55:50 +0700

    fix: file python not found

--- convert_ke_float.sh
+++ convert_ke_float.sh
@@ -6,6 +6,6 @@ for file in "final dataset"/*; do
         modified_name=$(basename "$file" | sed 's/Datasaet //' | sed 's/.xlsx//')
         final_name=$(echo "$modified_name" | tr ' ' '_' | tr '[:upper:]' '[:lower:]')
         final_name+="_float"
-        python preprocess.py -i "$file" -o "preprocessed/$final_name.csv"
+        python convert_to_float.py -i "$file" -o "floated/$final_name.csv"
     fi
 done
