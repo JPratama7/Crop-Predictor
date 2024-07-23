4df9ca475dc8ecbaa11beeac9048b3207654399f
JPratama7 <josepratama080@gmail.com>
2024-07-24 03:39:17 +0700

    feat: parse input

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,24 @@ import pandas as pd
 import argparse
 import sys
 
+from result import Result, Err, Ok
+
+
+def name_and_file_type(args: argparse.Namespace, key: str) -> Result[tuple[str, str], str]:
+    arg = vars(args)
+
+    if key not in arg:
+        return Err("Key not found")
+
+    arg_val = arg[key]
+
+    if arg_val.endswith('.xlsx'):
+        return Ok((arg_val, 'excel'))
+
+    if arg_val.endswith('.csv'):
+        return Ok((arg_val, 'csv'))
+
+    return Err("data type not supported")
 
 def main():
     parser = argparse.ArgumentParser(description='Proses input dan output file.')
@@ -15,14 +33,23 @@ def main():
 
     args = parser.parse_args()
 
-    df = pd.read_excel(args.input)
+    file_input = name_and_file_type(args, 'input')
+    output = name_and_file_type(args, 'output')
+
+    if file_input.is_err():
+        sys.stderr.write(file_input.unwrap_err())
+        sys.exit(1)
+
+    if output.is_err():
+        sys.stderr.write(output.unwrap_err())
+        sys.exit(1)
+
+    df = pd.DataFrame()
 
     # Normalize Kabupaten
     df['Kabupaten'] = df['Kabupaten'].str.replace('Kab. ', '')
     df['Kabupaten'] = df['Kabupaten'].str.replace('Kabupaten ', '')
 
-    print(df.isnull().sum())
-    print(df.isna().sum())
 
 if __name__ == "__main__":
     main()
